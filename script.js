ed76b29b1b15765919690b05daf5c756c16aa6aa
Yunus Ahmed <YunusAhmed1266@gmail.com>
2025-05-04 15:56:13 +0100

    create grid and resizing feature

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
-const container = document.querySelector("#container");
+let color = "black"
 
-for (let i = 0; i < 16; i++) {
-  for (let j = 0; j < 16; j++) {
-    const box = document.createElement("div");
+function fillContainer(size) {
+  let container = document.querySelector("#container");
+  let boxes = container.querySelectorAll("div");
+  boxes.forEach((div) => div.remove());
+  container.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+  container.style.gridTemplateRows = `repeat(${size} , 1fr)`;
+  
+  let amount = size*size
+  for (let i = 0; i < amount; i++) {
+    let box = document.createElement("div");
     box.classList.add("box");
     container.appendChild(box);
+    box.addEventListener("mouseover", hoverEffect);
+    container.insertAdjacentElement("beforeend", box);
   }
 }
+fillContainer(16)
 
-container.addEventListener("mouseover", () => {
-  const box = document.createElement("div");
-  box.classList.add("box");
-  box.style.backgroundColor = "red";
-}); 
+function hoverEffect() {
+  this.style.backgroundColor = color;
+}
 
-console.log(typeof box);
+function changeColor(choice) {
+  color = choice
+}
+
+function clearBoard() {
+  let container = document.querySelector("#container");
+  let boxes = container.querySelectorAll("div");
+  boxes.forEach((div) => div.style.backgroundColor = "white");
+}
+
+function changeSize(input) {
+  if (input >= 2 && input <= 100) {fillContainer(input)}
+}
